dd7ca2d2665ee160fda921a76ace6ab5bb8135b4
Evandro Carenho <ecarrenho@daitangroup.com>
2017-03-30 15:59:32 -0300

    Fixing JIRA configurations and removing unused gulp file.

--- src/main/webapp/js/config.service.js
+++ src/main/webapp/js/config.service.js
@@ -1,7 +1,7 @@
 export default ({
-  appTitle: 'Configure JIRASS',
-  appName: 'JIRAssss',
-  appSubTitle: 'JIRAsss Webhhok Integration',
+  appTitle: 'Configure JIRA',
+  appName: 'JIRA',
+  appSubTitle: 'JIRA Webhhok Integration',
   appIMShortHand: 'JIRA', // will display "One on One with JIRA"
   appToggleSetupInstructions: true,
   appLogo: 'jira_logo.png',
