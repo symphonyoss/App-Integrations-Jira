f3088055e2f2d24c7a962bbd48e086373b9735a2
pdarde-daitan <pdarde@daitangroup.com>
2017-04-03 15:29:51 -0300

    APP-931, fix typo

--- src/main/webapp/js/config.service.js
+++ src/main/webapp/js/config.service.js
@@ -1,7 +1,7 @@
 export default ({
   appTitle: 'Configure JIRA',
   appName: 'JIRA',
-  appSubTitle: 'JIRA Webhhok Integration',
+  appSubTitle: 'JIRA Webhook Integration',
   appIMShortHand: 'JIRA', // will display "One on One with JIRA"
   appToggleSetupInstructions: true,
   appLogo: 'jira_logo.png',
