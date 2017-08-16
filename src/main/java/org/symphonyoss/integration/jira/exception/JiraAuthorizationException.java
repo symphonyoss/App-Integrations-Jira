acb124f0616c1359cfa66cee37778a54a77c6aec
Alexandre Ribeiro da Silva <alexandre.ribeiro@symphony.com>
2017-08-16 17:28:49 -0300

    APP-1179: create javadoc to InvalidJiraURLException and JiraAuthorizationException

--- src/main/java/org/symphonyoss/integration/jira/exception/JiraAuthorizationException.java
+++ src/main/java/org/symphonyoss/integration/jira/exception/JiraAuthorizationException.java
@@ -3,6 +3,8 @@ package org.symphonyoss.integration.jira.exception;
 import org.symphonyoss.integration.exception.IntegrationRuntimeException;
 
 /**
+ * Unchecked exception thrown to indicate that JIRA authorization was failed.
+ *
  * Created by hamitay on 8/16/17.
  */
 public class JiraAuthorizationException extends IntegrationRuntimeException {
