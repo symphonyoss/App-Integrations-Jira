62b6a688e4d1d3f83f43fad80aa26827bb5ffdc0
Robson Vinícius Vieira Sanchez <rsanchez@symphony.com>
2017-08-18 17:49:24 -0300

    APP-1179 Unit tests

--- src/test/java/org/symphonyoss/integration/jira/api/JiraApiResourceTest.java
+++ src/test/java/org/symphonyoss/integration/jira/api/JiraApiResourceTest.java
@@ -1,38 +1,37 @@
 package org.symphonyoss.integration.jira.api;
 
 import static org.junit.Assert.assertEquals;
-import static org.mockito.Matchers.anyLong;
-import static org.mockito.Matchers.anyString;
 import static org.mockito.Mockito.doReturn;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.doThrow;
 
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.runners.MockitoJUnitRunner;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.symphonyoss.integration.authentication.api.jwt.JwtAuthentication;
 import org.symphonyoss.integration.authorization.AuthorizationException;
+import org.symphonyoss.integration.authorization.oauth.v1.OAuth1Exception;
 import org.symphonyoss.integration.authorization.oauth.v1.OAuth1Provider;
 import org.symphonyoss.integration.exception.IntegrationUnavailableException;
+import org.symphonyoss.integration.jira.exception.InvalidJiraURLException;
+import org.symphonyoss.integration.jira.exception.JiraAuthorizationException;
+import org.symphonyoss.integration.jira.exception.JiraUnexpectedException;
 import org.symphonyoss.integration.jira.services.SearchAssignableUsersService;
 import org.symphonyoss.integration.jira.services.UserAssignService;
 import org.symphonyoss.integration.jira.webhook.JiraWebHookIntegration;
-import org.symphonyoss.integration.logging.LogMessageSource;
-import org.symphonyoss.integration.logging.MessageUtils;
-import org.symphonyoss.integration.service.IntegrationBridge;
+import org.symphonyoss.integration.model.config.IntegrationSettings;
 
 import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 /**
  * Created by hamitay on 8/16/17.
  */
 @RunWith(MockitoJUnitRunner.class)
-@Ignore
 public class JiraApiResourceTest {
 
   private static String ISSUE_KEY = "issueKey";
@@ -43,11 +42,14 @@ public class JiraApiResourceTest {
 
   private static String AUTHORIZATION_HEADER = "authorizationHeader";
 
-  private static String JIRA_INTEGRATION_URL = "https://jiraIntegrationUrl";
+  private static String JIRA_INTEGRATION_URL = "https://test.symphony.com";
 
   private static String ACCESS_TOKEN = "accessToken";
 
   private static final Long USER_ID = 10L;
+
+  private static final String PATH_JIRA_API_SEARCH_USERS =
+      "rest/api/latest/user/assignable/search?issueKey=%s&username=%s&maxResults=%s";
 
   private JiraApiResource jiraApiResource;
 
@@ -67,20 +69,67 @@ public class JiraApiResourceTest {
   private OAuth1Provider provider;
 
   @Before
-  public void prepareMockResource() throws AuthorizationException {
+  public void prepareMockResource() throws AuthorizationException, MalformedURLException {
+    String pathApiJiraUsersSearch = String.format(PATH_JIRA_API_SEARCH_USERS, ISSUE_KEY,
+        USERNAME, null);
+
+    URL jiraBaseUrl = new URL(JIRA_INTEGRATION_URL);
+    URL assignableUserUrl = new URL(jiraBaseUrl, pathApiJiraUsersSearch);
+
+    doReturn(USER_ID).when(jwtAuthentication).getUserIdFromAuthorizationHeader(AUTHORIZATION_HEADER);
+    doReturn(new IntegrationSettings()).when(jiraWebHookIntegration).getSettings();
     doReturn(ACCESS_TOKEN).when(jiraWebHookIntegration).getAccessToken(JIRA_INTEGRATION_URL, USER_ID);
     doReturn(provider).when(jiraWebHookIntegration).getOAuth1Provider(JIRA_INTEGRATION_URL);
+
+    doReturn(new ResponseEntity(HttpStatus.OK)).when(searchAssignableUsersService)
+        .searchAssingablesUsers(ACCESS_TOKEN, provider, assignableUserUrl, ISSUE_KEY);
 
     jiraApiResource = new JiraApiResource(jiraWebHookIntegration, jwtAuthentication,
         userAssignService, searchAssignableUsersService);
   }
 
+  @Test(expected = IntegrationUnavailableException.class)
+  public void testSearchAssignableUserUnavailable() throws IOException {
+    doReturn(null).when(jiraWebHookIntegration).getSettings();
+
+    jiraApiResource.searchAssignableUsers(ISSUE_KEY, USERNAME, AUTHORIZATION_HEADER, JIRA_INTEGRATION_URL);
+  }
+
+  @Test(expected = JiraAuthorizationException.class)
+  public void testSearchAssignableUserNullAccessToken() throws IOException {
+    doReturn(0L).when(jwtAuthentication).getUserIdFromAuthorizationHeader(AUTHORIZATION_HEADER);
+    jiraApiResource.searchAssignableUsers(ISSUE_KEY, USERNAME, AUTHORIZATION_HEADER, JIRA_INTEGRATION_URL);
+  }
+
+  @Test(expected = JiraUnexpectedException.class)
+  public void testSearchAssignableUserAccessTokenFailed() throws IOException, AuthorizationException {
+    doThrow(AuthorizationException.class).when(jiraWebHookIntegration).getAccessToken(JIRA_INTEGRATION_URL, USER_ID);
+    jiraApiResource.searchAssignableUsers(ISSUE_KEY, USERNAME, AUTHORIZATION_HEADER, JIRA_INTEGRATION_URL);
+  }
+
+  @Test(expected = JiraAuthorizationException.class)
+  public void testSearchAssignableUserProviderFailed() throws IOException, AuthorizationException {
+    doThrow(OAuth1Exception.class).when(jiraWebHookIntegration).getOAuth1Provider(JIRA_INTEGRATION_URL);
+    jiraApiResource.searchAssignableUsers(ISSUE_KEY, USERNAME, AUTHORIZATION_HEADER, JIRA_INTEGRATION_URL);
+  }
+
+  @Test(expected = InvalidJiraURLException.class)
+  public void testSearchAssignableUserInvalidUrl() throws IOException, AuthorizationException {
+    String url = "test";
+
+    doReturn(ACCESS_TOKEN).when(jiraWebHookIntegration).getAccessToken(url, USER_ID);
+    doReturn(provider).when(jiraWebHookIntegration).getOAuth1Provider(url);
+
+    jiraApiResource.searchAssignableUsers(ISSUE_KEY, null, AUTHORIZATION_HEADER, url);
+  }
+
+  @Test
   public void testSearchAssignableUser() throws IOException {
     ResponseEntity expectedResponse = new ResponseEntity(HttpStatus.OK);
 
     ResponseEntity responseEntity =
-        jiraApiResource.searchAssignableUsers(ISSUE_KEY, USERNAME, CONFIGURATION_ID,
-            AUTHORIZATION_HEADER);
+        jiraApiResource.searchAssignableUsers(ISSUE_KEY, USERNAME, AUTHORIZATION_HEADER,
+            JIRA_INTEGRATION_URL);
 
     assertEquals(expectedResponse, responseEntity);
   }
