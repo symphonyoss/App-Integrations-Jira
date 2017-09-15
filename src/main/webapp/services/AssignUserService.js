baa963eaec5880c9d3ef52d588b92e52054934fb
Robson Vinícius Vieira Sanchez <rsanchez@symphony.com>
2017-09-15 17:32:19 -0300

    APP-1213 Create a enricher for 'assign ticket' JIRA's action - search for users dialog
    - API calls

--- src/main/webapp/services/AssignUserService.js
+++ src/main/webapp/services/AssignUserService.js
@@ -1,3 +1,6 @@
+import axios from 'axios';
+import { authorizeUser } from 'symphony-integration-commons';
+
 const assignDialog = require('../templates/assignDialog.hbs');
 const errorDialog = require('../templates/errorDialog.hbs');
 
@@ -5,6 +8,7 @@ export default class AssignUserService {
   constructor(serviceName) {
     this.serviceName = serviceName;
     this.selectedUser = {};
+    this.baseUrl = 'https://localhost.symphony.com:8186/integration';
   }
 
   showDialog(data) {
@@ -41,13 +45,75 @@ export default class AssignUserService {
     dialogsService.show('assignIssue', this.serviceName, template, userData, { title: 'Assign issue' });
   }
 
-  save(data) {
+  searchAssignableUser(url, issueKey, jwt) {
+    const apiUrl = `${this.baseUrl}/v1/jira/rest/api/user/assignable/search`;
+    const emailAddress = this.selectedUser.email;
+
+    const params = {
+      url,
+      issueKey,
+      username: emailAddress,
+    };
+
+    return axios.get(apiUrl, {
+      params,
+      headers: { Authorization: `Bearer ${jwt}` },
+    });
+  }
+
+  assignUser(url, issueKey, username, jwt) {
+    const apiUrl = `${this.baseUrl}/v1/jira/rest/api/issue/${issueKey}/assignee`;
+
+    const params = {
+      url,
+      username,
+    };
+
+    return axios.put(apiUrl, {
+      params,
+      headers: { Authorization: `Bearer ${jwt}` },
+    });
+  }
+
+  successDialog(issueKey) {
     const dialogsService = SYMPHONY.services.subscribe('dialogs');
 
     this.close('assignIssue');
 
-    const template = `<dialog><h1>Issue ${data.entity.issue.key} assigned to ${this.selectedUser.prettyName}</h1></dialog>`;
+    const template = `<dialog><h1>Issue ${issueKey} assigned to ${this.selectedUser.prettyName}</h1></dialog>`;
     dialogsService.show('userAssigned', this.serviceName, template, {}, { title: 'Assign issue' });
+  }
+
+  errorDialog() {
+    const dialogsService = SYMPHONY.services.subscribe('dialogs');
+
+    const template = `<dialog><h1>User ${this.selectedUser.prettyName} not authorized</h1></dialog>`;
+    dialogsService.show('errorAssignDialog', this.serviceName, template, {}, { title: 'Assign issue' });
+  }
+
+  save(data) {
+    const baseUrl = data.entity.baseUrl;
+    const issueKey = data.entity.issue.key;
+    let jwt;
+
+    authorizeUser(baseUrl)
+      .then((response) => {
+        if (response.success) {
+          jwt = response.jwt;
+          return this.searchAssignableUser(baseUrl, issueKey, jwt);
+        }
+
+        return Promise.reject(new Error('unauthorized'));
+      })
+      .then((users) => {
+        if (users.length === 0) {
+          this.errorDialog();
+          return Promise.reject(new Error('forbidden'));
+        }
+
+        return this.assignUser(baseUrl, issueKey, users[0].name, jwt);
+      })
+      .then(() => this.successDialog(issueKey));
   }
 
   close(dialog) {
